7fe771c1d7c7300e798c613089fc845487c1f69d
kausarahamed <galibhasan517@gmail.com>
2022-03-30 19:50:17 +0600

    nav responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./component/header/Header";
+import Navber from "./component/Navbar/Navber";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navber></Navber>
+      <Header></Header>
+      <h1 className="text-6xl">welcome to my pricing club</h1>
     </div>
   );
 }
